31260b5876d1a47b491c7587cbe914c922d00bc0
Wikijito7 <wikyfg@gmail.com>
2020-05-06 18:05:40 +0200

    Modificado keepit.py
    Añadida la clase Usuario.
    Añadido los setters y getters de Nota.

--- keepit.py
+++ keepit.py
@@ -1,24 +1,67 @@
-import pymysql
+# import pymysql
 from tkinter import *
 
-class Usuario:
-    pass
+# connection = pymysql.connect(host='localhost',
+#                              user='user',
+#                              password='passwd',
+#                              db='db',
+#                              charset='utf8mb4',
+#                              cursorclass=pymysql.cursors.DictCursor)
+
 
 class Nota:
-    def __init__(self, titulo, contenido, categoria, usuario, etiquetas = None):
+    def __init__(self, titulo, contenido, categoria, usuario, identificador, etiquetas = None):
         if isinstance(titulo, str) and isinstance(categoria, str) and isinstance(contenido, str) and isinstance(usuario, Usuario):
             self.titulo = titulo
-            self.categoria = categoria
+            self.categoria = categoria # ""
             self.contenido = contenido
             self.usuario = usuario
+            self.identificador = identificador
             if etiquetas == None:
-                self.etiquetas = []
+                self.etiquetas = [] # []
             elif isinstance(etiquetas, list):
                 self.etiquetas = etiquetas
             else:
                 raise ValueError("Las etiquetas deben venir dadas con una lista.")
         else:
             raise ValueError
+
+    #getter and setters for Nota
+    def get_etiquetas(self):
+        return self.etiquetas
+
+    def set_etiquetas(self, nuevas_etiquetas: list):
+        if isinstance(nuevas_etiquetas, list):
+            self.etiquetas = nuevas_etiquetas
+
+    def add_etiqueta(self, etiqueta: str):
+        if isinstance(etiqueta, str) and etiqueta not in self.etiquetas:
+            self.etiquetas.append(etiqueta)
+
+
+            
+        
+    def get_categoria(self):
+        return self.categoria
+
+
+    def set_categoria(self, nueva_categoria: str):
+        if isinstance(nueva_categoria, str):
+            self.categoria = nueva_categoria
+        
+
+class Usuario:
+    def __init__(self, email, password):
+        if isinstance(email, str) and ininstance(password, str):
+            self.email = email
+            self.password = password
+    
+    def __str__(self):
+        return f"Usuario('email':{self.email}, 'password':{self.password})"
+    
+    def get_email(self):
+        return self.email
+
 
 """
 https://python-textbok.readthedocs.io/en/1.0/Introduction_to_GUI_Programming.html
@@ -89,4 +132,18 @@ Button(master, text="Close", command=master.quit)
     -> command: lo que se ejecuta al hacerle click
 
 btnFiltrar = Button(master, text="paula quejica", command=filtrar(palabras_clave))
+"""
+
+
+
+"""
+    loquesea = consultamysql -> 15*(notas)
+    Nota(contenido que hemos obtenido de la consulta)
+
+    a. Al cargar el programa, se generan las notas del usuario. Filtrado, borrado, etc se hace en codigo interno. Al cerrar o periodicamente
+        se le informa a la base.
+        TODO: añadir botón guardar en la nube.
+
+    b. No hay notas, cargamos temporalmente todas las notas. Filtrado -> borramos notas, hacemos consulta para generar nuevos objetos
+        Nota. Cada vez que hay modificaciones, se crean o eliminan notas, hay que informarle a la base.
 """
